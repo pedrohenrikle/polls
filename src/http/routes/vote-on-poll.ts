9d14a74a957646ff76a06ff3fe7a08111faf400e
pedrohenrikle <pedro.klein.sl@gmail.com>
2024-02-06 11:01:15 -0300

    ci: create get-poll, vote-poll routes and add @fastify/cookies

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,38 @@
+import {z} from "zod"
+import { randomUUID } from "node:crypto"
+import { prisma } from "../../lib/prisma"
+import { FastifyInstance } from "fastify"
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post('/polls/:pollId/votes', async (request, reply) => {
+    // Parâmetro do corpo da requisição
+    const voteOnPollBody = z.object({
+      pollOptionId: z.string().uuid()
+    })
+
+    // Parâmetro da URL
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid()
+    })
+    
+    const { pollOptionId } = voteOnPollBody.parse(request.body)
+    const { pollId } = voteOnPollParams.parse(request.params)
+
+    let { sessionId } = request.cookies
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        signed: true,
+        httpOnly: true
+      })
+  
+    }
+
+
+    return reply.status(201).send()
+  })
+}
